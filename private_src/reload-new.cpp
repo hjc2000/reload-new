dbfff3c63b534eb3cb6b79ebba4fbd9985988a4b
hjc2000 <hjc.666@foxmail.com>
2025-04-02 09:38:02 +0800

    new

---
+++ private_src/reload-new.cpp
@@ -0,0 +1,95 @@
+#include "bsp-interface/di/heap.h"
+
+void *operator new(size_t size)
+{
+	void *ret = bsp::di::heap::Malloc(size);
+	if (ret == nullptr)
+	{
+		throw std::bad_alloc{};
+	}
+
+	return ret;
+}
+
+void *operator new[](size_t size)
+{
+	void *ret = bsp::di::heap::Malloc(size);
+	if (ret == nullptr)
+	{
+		throw std::bad_alloc{};
+	}
+
+	return ret;
+}
+
+void *operator new(size_t size, std::nothrow_t const &) noexcept
+{
+	void *ret = bsp::di::heap::Malloc(size);
+	return ret;
+}
+
+void *operator new[](size_t size, std::nothrow_t const &) noexcept
+{
+	void *ret = bsp::di::heap::Malloc(size);
+	return ret;
+}
+
+void operator delete(void *ptr) noexcept
+{
+	if (ptr == nullptr)
+	{
+		return;
+	}
+
+	bsp::di::heap::Free(ptr);
+}
+
+void operator delete[](void *ptr) noexcept
+{
+	if (ptr == nullptr)
+	{
+		return;
+	}
+
+	bsp::di::heap::Free(ptr);
+}
+
+void operator delete(void *ptr, std::nothrow_t const &) noexcept
+{
+	if (ptr == nullptr)
+	{
+		return;
+	}
+
+	bsp::di::heap::Free(ptr);
+}
+
+void operator delete[](void *ptr, std::nothrow_t const &) noexcept
+{
+	if (ptr == nullptr)
+	{
+		return;
+	}
+
+	bsp::di::heap::Free(ptr);
+}
+
+void operator delete(void *ptr, size_t size) noexcept
+{
+	if (ptr == nullptr)
+	{
+		return;
+	}
+
+	bsp::di::heap::Free(ptr);
+}
+
+void operator delete[](void *ptr, size_t size) noexcept
+{
+	if (ptr == nullptr)
+	{
+		return;
+	}
+
+	bsp::di::heap::Free(ptr);
+}
---
+++ project.cmake
@@ -0,0 +1,3 @@
+add_library(${ProjectName} OBJECT)
+target_import_src(${ProjectName})
+target_import_bsp_interface(${ProjectName} PUBLIC)
