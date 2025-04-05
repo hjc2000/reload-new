68c4dfcd3902bf38f9df04d7a29705c218e0f0b8
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-05 22:58:42 +0800

    realloc

--- private_src/reload-new.cpp
+++ private_src/reload-new.cpp
@@ -1,4 +1,9 @@
 #include "bsp-interface/di/heap.h"
+#include "bsp-interface/di/task.h"
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <map>
 
 void *operator new(size_t size)
 {
@@ -92,4 +97,73 @@ void operator delete[](void *ptr, size_t size) noexcept
 	}
 
 	bsp::di::heap::Free(ptr);
+}
+
+namespace
+{
+	std::map<void *, size_t> _size_map{};
+}
+
+extern "C"
+{
+	void *malloc(size_t size)
+	{
+		bsp::di::task::TaskGuard g{};
+		void *ret = bsp::di::heap::Malloc(size);
+		if (ret != nullptr)
+		{
+			_size_map[ret] = size;
+		}
+
+		return ret;
+	}
+
+	void free(void *ptr)
+	{
+		if (ptr == nullptr)
+		{
+			return;
+		}
+
+		bsp::di::task::TaskGuard g{};
+		bsp::di::heap::Free(ptr);
+		_size_map.erase(ptr);
+	}
+
+	void *realloc(void *ptr, size_t new_size)
+	{
+		// 特殊情况：如果新大小为 0，相当于 free.
+		if (new_size == 0)
+		{
+			free(ptr);
+			return nullptr;
+		}
+
+		// 特殊情况：如果 ptr 为 nullptr, 相当于 malloc.
+		if (ptr == nullptr)
+		{
+			return malloc(new_size);
+		}
+
+		void *old_mem = ptr;
+		size_t old_size = 0;
+
+		{
+			bsp::di::task::TaskGuard g{};
+			auto it = _size_map.find(ptr);
+			if (it != _size_map.end())
+			{
+				old_size = it->second;
+			}
+		}
+
+		void *new_mem = malloc(new_size);
+
+		std::copy(reinterpret_cast<uint8_t *>(old_mem),
+				  reinterpret_cast<uint8_t *>(old_mem) + std::min(old_size, new_size),
+				  reinterpret_cast<uint8_t *>(new_mem));
+
+		free(old_mem);
+		return new_mem;
+	}
 }
